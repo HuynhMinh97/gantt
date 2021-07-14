7605357965f389c4926070f01ef654ec6d59473d
Dan Nguyen Nhat Truong <truongdnn@aureole-it.vn>
2021-07-14 15:05:18 +0700

    fix base component cant get lang

--- libs/ui/src/lib/components/base.component.ts
+++ libs/ui/src/lib/components/base.component.ts
@@ -62,10 +62,11 @@ export class AitBaseComponent implements OnInit, OnDestroy {
     public apollo: Apollo,
     public userService: AitUserService,
     _env: AitEnvironmentService,
-    private layoutScrollService?: NbLayoutScrollService,
+    public layoutScrollService?: NbLayoutScrollService,
     public toastrService?: NbToastrService,
   ) {
     this.user_id = AitAppUtils.getUserId();
+    const userId = this.authService.getUserID();
     this.env = _env;
     this.company = this.env.COMMON.COMPANY_DEFAULT;
     // call api get all message follow by type as I : Information , W : Warning, E: Error
@@ -75,30 +76,6 @@ export class AitBaseComponent implements OnInit, OnDestroy {
     this.getCommonCaptions().then();
 
 
-    // Listening event loading when the app is loading
-    store
-      .pipe(select(getLoading))
-      .subscribe((loading) => (this.isLoading = loading));
-
-
-
-
-
-    // apply user info for base component
-    store.subscribe({
-      next: state => {
-        const { userInfo } = state.commonReducer;
-        this.user_id = AitAppUtils.getUserId();
-        this.username = userInfo?.username;
-        this.userProfile = userInfo?.user_profile;
-        this.email = userInfo?.email;
-      }
-    });
-
-  }
-
-  public initBaseComponent = () => {
-    const userId = this.authService.getUserID();
     //setting default lang & company
 
     this.store.pipe(select(getLang)).subscribe(lang => {
@@ -194,6 +171,31 @@ export class AitBaseComponent implements OnInit, OnDestroy {
 
       }
     }
+
+    // Listening event loading when the app is loading
+    store
+      .pipe(select(getLoading))
+      .subscribe((loading) => (this.isLoading = loading));
+
+
+
+
+
+    // apply user info for base component
+    store.subscribe({
+      next: state => {
+        const { userInfo } = state.commonReducer;
+        this.user_id = AitAppUtils.getUserId();
+        this.username = userInfo?.username;
+        this.userProfile = userInfo?.user_profile;
+        this.email = userInfo?.email;
+      }
+    });
+
+  }
+
+  public initBaseComponent = () => {
+
 
     if (localStorage.getItem('refresh_token')) {
       this.refreshToken().then((r: any) => {
