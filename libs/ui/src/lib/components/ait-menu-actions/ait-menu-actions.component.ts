906ed84804db889950e56cb5f4038e8ad0fad744
Dan Nguyen Nhat Truong <truongdnn@aureole-it.vn>
2021-07-27 14:35:01 +0700

    fix datepicker, usermenu

--- libs/ui/src/lib/components/ait-menu-actions/ait-menu-actions.component.ts
+++ libs/ui/src/lib/components/ait-menu-actions/ait-menu-actions.component.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { NbIconLibraries } from '@nebular/theme';
 import { select, Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
 import { AitTranslationService } from '../../services';
 import { AitLayoutService } from '../../services/common/ait-layout.service';
-import { AppState, getCaption } from '../../state/selectors';
+import { AppState, getCaption, getLang } from '../../state/selectors';
 
 
 @Component({
@@ -14,6 +15,10 @@ import { AppState, getCaption } from '../../state/selectors';
   styleUrls: ['./ait-menu-actions.component.scss']
 })
 export class AitMenuActionsComponent implements OnInit {
+  screens = ['candidates', 'jobs', '/']
+  currentScreen = '/';
+
+  screenCurrent: BehaviorSubject<any> = new BehaviorSubject('/');
   constructor(
     private layoutService: AitLayoutService,
     private translateService: AitTranslationService,
@@ -21,15 +26,54 @@ export class AitMenuActionsComponent implements OnInit {
     private router: Router,
     private iconLibraries: NbIconLibraries
   ) {
+    if (!localStorage.link) {
+      localStorage.setItem('link', this.currentScreen);
+    }
+    else {
+      this.currentScreen = localStorage.link;
+    }
     this.iconLibraries.registerFontPack('font-awesome', { packClass: 'far', iconClassPrefix: 'fa' });
-  }
-  buttons = [];
-  @Input() menu_actions: any[] = [];
-  ngOnInit() {
     if (!this.menu_actions) {
       this.menu_actions = []
     }
+
+    router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        if (location.hash.includes(this.screens[0])) {
+          this.currentScreen = '/candidates';
+          this.screenCurrent.next('/candidates')
+        }
+        else if (location.hash.includes(this.screens[1])) {
+          this.currentScreen = '/jobs'
+          this.screenCurrent.next('/jobs')
+
+        }
+        else if (location.hash === this.screens[2]) {
+          this.currentScreen = '/';
+          this.screenCurrent.next('/')
+
+        }
+        else {
+          this.screenCurrent.next('/other');
+        }
+
+      }
+    })
+
+
     this.store.pipe(select(getCaption)).subscribe(() => {
+      this.applyButtons();
+    })
+  }
+  buttons = [];
+  @Input() menu_actions: any[] = [];
+
+  ngOnInit() {
+    this.applyButtons();
+  }
+
+  applyButtons = () => {
+    this.screenCurrent.subscribe(val => {
       const target: any = [
         ...this.menu_actions
 
@@ -40,18 +84,33 @@ export class AitMenuActionsComponent implements OnInit {
           if (m.pack) {
             obj = { pack: m.pack }
           }
+
           return {
             ...m,
             title: this.translateService.translate(m.title),
             icon: {
               icon: m.icon,
-              ...obj
+              ...obj,
+              status: m.link === val ? 'primary' : null
             }
           }
         }
         return m
       })
     })
+  }
+
+  swipeScreen = (link: string) => {
+    this.currentScreen = link;
+    this.screenCurrent.next(link);
+    localStorage.setItem('link', link);
+    this.buttons = this.buttons.map(b => ({ ...b, icon: { ...b.icon, status: null } }))
+    const buts = this.buttons.find(b => b.link === link);
+    if (buts) {
+      buts.icon.status = 'primary';
+    }
+    this.navigate(this.currentScreen);
+
   }
 
   navigate = (link: string) => this.router.navigate([link]);
