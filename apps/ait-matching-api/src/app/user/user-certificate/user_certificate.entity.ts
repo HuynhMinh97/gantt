83b3e745f193702d90bccfe0f354675d30b5f3c5
thuannv <thuannv@aureole-it.vn>
2021-09-10 08:55:13 +0700

    edit cerfiticate

--- apps/ait-matching-api/src/app/user/user-certificate/user_certificate.entity.ts
+++ apps/ait-matching-api/src/app/user/user-certificate/user_certificate.entity.ts
@@ -7,33 +7,24 @@ export class UserCertificateEntity extends BaseEntity {
   name?: string;
 
   @Field(() => String, { nullable: true })
-  certificate?: string;
+  certificate_award_number?: string;
 
   @Field(() => String, { nullable: true })
   grade?: string;
 
   @Field(() => String, { nullable: true })
-  prefecture?: string;
+  issue_by?: string;
+
+  @Field(() => Float, { nullable: true })
+  issue_date_from?: number;
+
+  @Field(() =>Float, { nullable: true })
+  issue_date_to?: number;
 
   @Field(() => String, { nullable: true })
-  issue?: string;
-
-  @Field(() => Float, { nullable: true })
-  issueDate?: number;
-
-  @Field(() => Float, { nullable: true })
-  immigration?: number;
+  description?: string;
 
   @Field(() => [String], { nullable: true })
   file?: string[];
-
-  @Field(() => String, { nullable: true })
-  description?: string; 
-
-  @Field(() => String, { nullable: true })
-  keyName?: string;
-  
-  @Field(() => String, { nullable: true })
-  id?: string;
 
 }
--- apps/ait-matching-webapp/src/app/services/user-certificate.service.ts
+++ apps/ait-matching-webapp/src/app/services/user-certificate.service.ts
@@ -10,64 +10,28 @@ export class UserCerfiticateService extends AitBaseService {
   saveUserCartificate = async (data: any[]) => {
     return await this.mutation('saveUsercertificate', 'user_certificate_award', data, { _key: true });
   }
-  saveMCartificate = async (data: any[]) => {
-    return await this.mutation('saveUsercertificate', 'm_certificate_award', data, { _key: true });
-  }
-  
-  findUserByKey = async (user_key : string, table : string) => {
-    // const condition = {
-    //   _key: user_key
-    // }
-    // const specialKeys = [
-    //   'issue'];
-
-    // specialKeys.forEach(item => {
-    //   condition[item] = {
-    //     attribute: item,
-    //     ref_collection: 'sys_master_data',
-    //     ref_attribute: 'code'
-    //   }
-    // })
-    // return await this.query('findUsercertificate', {
-    //   collection: 'user_certificate_award',
-    //   condition
-    // }, {
-    //   _key : true,
-    //   name:true,
-    //   certificate: true,
-    //   grade: true,
-    //   issue: {
-    //     _key : true,
-    //     value : true
-    //   },
-    //   issueDate: true,
-    //   immigration: true,
-    //   description: true,
-    //   file: true,
-    // })  
+  findUserByKey = async (user_key : string) => {
     const condition = {
-      id: user_key,
+      _key: user_key,
       del_flag: false
     }
-    return await this.query('findUsercertificate', {collection: table,  condition    }, 
+    return await this.query('findUsercertificate', {collection: 'user_certificate_award',  condition    }, 
     {
       _key : true,
       name:true,
-      certificate: true,
+      certificate_award_number: true,
       grade: true,
-      issue: true,
-      issueDate: true,
-      immigration: true,
+      issue_by: true,
+      issue_date_from: true,
+      issue_date_to: true,
       description: true,
       file: true,
-      keyName:true,
-      id:true,
     })
   }
 
-  deleteUserByKey = async (_key: string , table:string) => {
+  remove = async (_key: string ) => {
     const returnFields = {_key: true };
     const data = { _key };
-    return await this.mutation('removeUsercertificate', table, [data], returnFields);
+    return await this.mutation('removeUsercertificate', 'user_certificate_award', [data], returnFields);
   }
 }
