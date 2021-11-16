ddfcab8ddda3ee7ffbe065d91198a4a29374d7db
thuannv <thuannv@aureole-it.vn>
2021-11-16 13:04:54 +0700

    update environment

--- apps/ait-api-starter/src/environments/environment.ts
+++ apps/ait-api-starter/src/environments/environment.ts
@@ -22,8 +22,8 @@ export const environment = {
   },
   DATABASE: {
     HOST: 'http://192.168.136.17:8529/',
-    NAME: 'ait_starter',
-    USER: 'ait_starter',
-    PASS: 'ait_starter',
+    NAME: 'ait_matching_dev',
+    USER: 'ait_matching_dev',
+    PASS: 'ait_matching_dev',
   },
 };
