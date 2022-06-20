0466474322ec25050cb563ee528423ed3272ef67
mannq <mannq@aureole-it.vn>
2022-06-20 17:22:27 +0700

    update save-temp

--- libs/ui/src/lib/components/ait-chip/ait-chip.component.ts
+++ libs/ui/src/lib/components/ait-chip/ait-chip.component.ts
@@ -87,9 +87,11 @@ export class AitChipComponent implements OnChanges {
 
   getTitle(title: string) {
     if (!this.isHaveBorder || title.length < 11) {
-      return title;
+      return title[0].toUpperCase() + title.slice(1);
     } else {
-      return title.substring(0, 7) + '...' + title.substring(title.length - 4);
+      const text =
+        title.substring(0, 7) + '...' + title.substring(title.length - 4);
+      return text[0].toUpperCase() + text.slice(1);
     }
   }
 }
